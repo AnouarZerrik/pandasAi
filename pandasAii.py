69193ea0d3b7fc5475c83d0aa95946553e1b6af8
UTENTE <UTENTE@ANZER>
2024-01-10 16:55:49 +0100

    ok

--- pandasAii.py
+++ pandasAii.py
@@ -7,7 +7,15 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-api =  os.getenv('GOOGLE_API_KEY')
+api = os.getenv('GOOGLE_API_KEY')
+
+i = 0
+def talk():
+        user_input = st.text_input('Talk with DataFrame...', key=f"input_{i}") # pass a unique key argument
+        if st.button('Execute', key=f"button_{i}"): # also pass a unique key argument to st.button
+            with st.spinner('Thinking...'):
+                st.info(agent.run(user_input))
+            
 
 
 llm = GoogleGenerativeAI(
@@ -22,8 +30,15 @@ if csv is not None:
     df = pd.read_csv(csv)
     st.dataframe(df.head(5))
     agent = create_pandas_dataframe_agent(llm, df, verbose=True)
-    input = st.text_input('Talk with DataFrame...')
-    btn = st.button('Execute')
-if btn:
-    with st.spinner('Thinking...'):
-        st.info(agent.run(input))
+    #input = st.text_input('Talk with DataFrame...')
+    #btn = st.button('Execute')
+
+    talk()
+# if btn:
+#     with st.spinner('Thinking...'):
+#         # I added a try-except block to handle possible errors
+#         try:
+#             st.info(agent.run(input))
+#             talk()
+#         except Exception as e:
+#             st.error(f"An error occurred: {e}")
